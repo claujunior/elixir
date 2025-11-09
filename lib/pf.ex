9e8e38e1ccdc1dd64534103c6d7a90f924e81b2d
claujunior <claudivanjunior2002@hotmail.com>
2025-11-09 00:52:34 -0300

    inicio validade

--- lib/pf.ex
+++ lib/pf.ex
@@ -8,9 +8,45 @@ defmodule Pf do
       |> Enum.with_index(0)
       |> Enum.map(fn {linha, idx} -> {idx, List.to_tuple(linha)} end)
       |> Enum.into(%{})
+
+      validade(map_linhas)
       rescue
         e in File.Error ->
           IO.puts("Erro ao abrir o arquivo: #{e.reason}")
       end
+    end
+
+    def validade(mapa) do
+  if tamanho(mapa) == :ok do
+    oresto(mapa)
+  else
+    "Entrada invalida!"
+  end
+end
+
+    def tamanho(mapa) do
+      {x,y} = mapa[0]
+      mapa_sem_primeiro = Map.drop(mapa, [0])
+      Enum.reduce_while(mapa_sem_primeiro, :ok, fn {_, {posix,posiy,largura,altura,_}}, _ ->
+        if String.to_integer(x) < String.to_integer(posix) + String.to_integer(altura) or
+            String.to_integer(y) < String.to_integer(posiy) + String.to_integer(largura) do
+            {:halt, :erro}
+        else
+          {:cont, :ok}
+        end
+      end)
+    end
+
+    def oresto(mapa,k) do
+      {x,y,xlarg,ylarg,_} = mapa[k]
+      mapa_sem_primeiro = Map.drop(mapa,[k],[0])
+      Enum.reduce_while(mapa_sem_primeiro, :ok, fn {_, {posix,posiy,largura,altura,_}}, _ ->
+        if String.to_integer(x) == String.to_integer(posix)  or
+            String.to_integer(y) == String.to_integer(posiy) do
+            {:halt, :erro}
+        else
+          {:cont, :ok}
+        end
+      end)
     end
 end
