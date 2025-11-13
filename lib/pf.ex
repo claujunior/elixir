c56cd79af2a9d259388bbc512b60688ccdb15a3c
claujunior <claudivanjunior2002@hotmail.com>
2025-11-12 22:29:54 -0300

    cansei

--- lib/pf.ex
+++ lib/pf.ex
@@ -5,6 +5,7 @@ defmodule Pf do
 
       map_linhas =
         Stream.map(stream, &String.trim/1)
+        |> Stream.reject(&(&1 == ""))
         |> Stream.map(&String.split/1)
         |> Enum.with_index(0)
         |> Enum.map(fn {linha, idx} -> {idx, List.to_tuple(linha)} end)
@@ -25,25 +26,26 @@ defmodule Pf do
         end)
 
       novo = Map.merge(mapa, kkk)
-
-      checar_valores(String.to_integer(x), String.to_integer(y),mapa,novo)
+      checar_valores(String.to_integer(x), String.to_integer(y), novo)
     rescue
       e in File.Error ->
         IO.puts("Erro ao abrir o arquivo: #{e.reason}")
     end
   end
-  def checar_valores(x, y, mapa, novo) do
-  cond do
-    x >= 100 or y >= 100 ->
-      "Entrada inválida: tamanho do tabuleiro excede o permitido"
 
-    map_size(mapa) > 128 ->
-      "Entrada inválida: blocos ultrapassam a quantidade permitida"
+  def checar_valores(x, y, mapa) do
+    cond do
+      x >= 100 or y >= 100 ->
+        "Entrada inválida: tamanho do tabuleiro excede o permitido"
 
-    true ->
-      validade(novo)
+      map_size(mapa) > 128 ->
+        "Entrada inválida: blocos ultrapassam a quantidade permitida"
+
+      true ->
+        validade(mapa)
+    end
   end
-end
+
   def validade(mapa) do
     if tamanho(mapa) == :ok do
       if aux2(aux(mapa)) == 1 do
@@ -57,7 +59,7 @@ end
         if aux2(usar) == 0 do
           "Entrada invalida, letra desconhecida"
         else
-          bfs(mapa, &acharvizinhos/1,&objetivo/1)
+          bfs(mapa, &acharvizinhos/1, &objetivo/1)
         end
       else
         "Entrada invalida, blocos sobrepostos"
@@ -93,24 +95,33 @@ end
     end)
   end
 
+  def oresto(mapa, _) when map_size(mapa) <= 1 do
+    :ok
+  end
   def oresto(mapa, k) do
-    {x, y, larg, altu, _} = mapa[k]
-    mapa_sem_primeiro = Map.drop(mapa, [0])
-    mapa_sem_dois = Map.drop(mapa_sem_primeiro, [k])
+    case Map.get(mapa, k) do
+      nil ->
+        :ok
 
-    if map_size(mapa_sem_dois) == 0 do
-      :xdd
-    else
-      Enum.reduce_while(mapa_sem_dois, :ok, fn {_, {posix, posiy, largura, altura, _}}, _ ->
-        if x + altu <= posix or
-             posix + altura <= x or
-             y + larg <= posiy or
-             posiy + largura <= y do
-          {:cont, :xdd}
-        else
-          {:halt, :erro}
+      {x, y, larg, altu, _} ->
+        mapa_sem_k = Map.drop(mapa, [k])
+
+        resultado =
+          Enum.reduce_while(mapa_sem_k, :ok, fn {_, {x2, y2, larg2, altu2, _}}, _ ->
+            if x + larg <= x2 or
+                 x2 + larg2 <= x or
+                 y + altu <= y2 or
+                 y2 + altu2 <= y do
+              {:cont, :ok}
+            else
+              {:halt, :erro}
+            end
+          end)
+
+        case resultado do
+          :ok -> oresto(mapa_sem_k, k + 1)
+          :erro -> :erro
         end
-      end)
     end
   end
 
@@ -118,7 +129,7 @@ end
     mapa_sem_zero = Map.drop(mapa, [0])
 
     Enum.map(mapa_sem_zero, fn {k, _} ->
-      oresto(mapa, k)
+      oresto(mapa_sem_zero, k)
     end)
   end
 
@@ -169,11 +180,16 @@ end
   end
 
   def valido(mapax1, mapax2, mapay1, mapay2, k) do
+    mapaxx1 = Map.drop(mapax1, [0])
+    mapaxx2 = Map.drop(mapax2, [0])
+    mapayy1 = Map.drop(mapay1, [0])
+    mapayy2 = Map.drop(mapay2, [0])
+
     [
-      auxvalido(tamanho(mapax1), oresto(mapax1, k)),
-      auxvalido(tamanho(mapax2), oresto(mapax2, k)),
-      auxvalido(tamanho(mapay1), oresto(mapay1, k)),
-      auxvalido(tamanho(mapay2), oresto(mapay2, k))
+      auxvalido(tamanho(mapax1), oresto(mapaxx1, k)),
+      auxvalido(tamanho(mapax2), oresto(mapaxx2, k)),
+      auxvalido(tamanho(mapay1), oresto(mapayy1, k)),
+      auxvalido(tamanho(mapay2), oresto(mapayy2, k))
     ]
   end
 
@@ -190,7 +206,7 @@ end
   end
 
   def auxvalido(a, k) do
-    if a == :ok and k == :xdd do
+    if a == :ok and k == :ok do
       :ok
     else
       :noop
@@ -204,7 +220,7 @@ end
     y1 + larg1 == largura + 1
   end
 
-    # Função pública
+
   def bfs(start, graph_fun, objetivo_fun) do
     queue = :queue.from_list([{start, [start]}])
     visitados = MapSet.new([start])
